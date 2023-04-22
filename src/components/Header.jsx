831414554af131429dc241d3a05c0d64176914cd
Mohamed Aziz Bellaaj <mohamedaziz.bellaaj@insat.ucar.tn>
2023-04-22 16:20:34 +0100

    fix: show logo image in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { TrollFace } from '/src/assets/troll-face.png';
+import TrollFace from '../assets/troll-face.png';
 
 export default function Header() {
   return (
     <header className='header'>
       <img
         src={TrollFace}
+        alt='troll face'
         className='header--image'
       />
       <h2 className='header--title'>Meme Generator</h2>
